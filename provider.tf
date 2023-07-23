27081f9aee83b37990e6070f671090c71b89b18a
erreyesarroyo <85759551+erreyesarroyo@users.noreply.github.com>
2023-07-23 19:46:12 -0400

    provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+# Not using keys or anything special. That would be beyond the scope here. Run AzLogin if needed
+
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
